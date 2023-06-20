0d04bcf17d130e990eee980e97e6b8e5be3a7408
stc-s22001 <s22001@suntech.jp>
2023-06-20 10:08:56 +0900

    Initial commit

---
+++ app/src/main/java/s22001/com/example/myhallo/MainActivity.java
@@ -0,0 +1,14 @@
+package s22001.com.example.myhallo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
